f65e0481dd094f87e24a6a9a7fa4becb5ba3eecd
Abdelrahman Mohamed <abdelrahman20205050@gmail.com>
2026-07-08 15:56:24 +0300

    done

---
+++ stream.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv(r"C:\Users\Hp\OneDrive\Documents\IEEE\Streamlit\IceCreamSales-temperatures.csv")
+
+X = df[["Temperature"]]
+y = df["Ice Cream Profits"]
+
+model = LinearRegression()
+model.fit(X, y)
+
+st.title("Ice Cream Profit Prediction")
+
+temp = st.number_input("Temperature")
+
+if st.button("Predict"):
+    profit = model.predict([[temp]])[0]
+    st.success(f"Predicted Profit = {profit:.2f}")
